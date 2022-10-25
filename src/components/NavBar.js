2f08d92bc5a1a3ea18e028d0e47359354c063bad
volausson <veronicagolausson@gmail.com>
2022-10-25 18:22:09 +0000

    srta add css in React

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,30 @@
-import React from 'react'
-import {Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/logo.jpg';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.io.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-  <Navbar expand="md" fixed="top">
-    <Container>
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
         <Navbar.Brand>
-          <img src={logo} alt='logo' height='45' />
+          <img src={logo} alt="logo" height="45" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-left">
-                <Nav.Link>
-                    <i className='fas fa-home'></i>Home
-                </Nav.Link>
-                <Nav.Link>
-                    <i className='fas fa-sign-in-alt'></i>Sign in
-                </Nav.Link>
-                <Nav.Link>
-                    <i className='fas fa-user-plus'></i>Sign up
-                </Nav.Link>
-            </Nav>
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
-    </Container>
+      </Container>
     </Navbar>
   );
 };
