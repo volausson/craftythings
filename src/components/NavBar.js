0dcfcd638c4f15945291b00d8a5bde5c16f749b2
volausson <veronicagolausson@gmail.com>
2022-10-27 13:58:12 +0000

    install react-router-dom and add routing to handle urls

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,41 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.io.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+        <NavLink to='/'>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        </Navbar.Brand></NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Activ}
+              to='/'
+              >
               <i className="fas fa-home"></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Activ}
+              to='/signin'
+              >
               <i className="fas fa-sign-in-alt"></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink 
+              className={styles.NavLink}
+              activeClassName={styles.Activ}
+              to='/signup'
+              >
               <i className="fas fa-user-plus"></i>Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
