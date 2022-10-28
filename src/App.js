d9e883875a3e27581d5cd3d4c3d9177a65e98de1
volausson <veronicagolausson@gmail.com>
2022-10-28 17:01:52 +0000

    add eventhandlers to signupform

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
-import{Route, Switch} from 'react-router-dom';
-import './api.axiosDefaults';
+import { Route, Switch } from 'react-router-dom';
+import "./api/axiosDefaults";
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -12,10 +13,9 @@ function App() {
         <Switch>
           <Route exact path='/' render={() => <h1>Home page</h1>} />
           <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
-          <Route exact path='/signup' render={() => <h1>Sign Up</h1>} />
+          <Route exact path='/signup' render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found</p>} />
         </Switch>
-        
       </Container>
     </div>
   );
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://cic-api.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axiox.defaults.withCredentials = true;
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
